d5e7dfacc633fb32f8fd4d49977604501733a98a
renanbarrostech-cmd <renanbarros.tech@gmail.com>
2026-09-05 08:34:29 -0300

    refactor: update Home and Auth pages for improved session handling and remove unused components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,28 @@
-import { HomeView } from "./_components/home-view";
+import { redirect } from "next/navigation";
+import { authClient } from "@/app/_lib/auth-client";
+import { headers } from "next/headers";
+import { getHomeData } from "./_lib/api/fetch-generated";
+import dayjs from "dayjs";
 
-export default function Home() {
-  return <HomeView />;
+
+export default async function Home() {
+  const session = await authClient.getSession({
+    fetchOptions: {
+      headers: await headers(),
+    }
+  })
+
+  if (!session.data?.user) redirect("/auth");
+    
+    const homeData = await getHomeData(dayjs().format("YYYY-MM-DD"))
+
+    console.log( {homeData})
+
+  return (
+    <div className="flex min-h-svh items-center justify-center">
+      <p>Home</p>
+    </div>
+  )
 }
+
+
